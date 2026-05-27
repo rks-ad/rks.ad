c5558ae5495304cbff050a1513ad4b37253155a5
rks-ad <iam@rks.ad>
2026-05-27 18:52:02 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export', // This is the missing piece
+  output: 'export',
   typescript: {
     ignoreBuildErrors: true,
   },
